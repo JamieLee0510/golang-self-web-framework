71465b8a5f02019acf4c9ecc24a3e4e56ca3690d
Ding <walker20665@gmail.com>
2022-01-11 10:20:05 +0800

    UPDATE:basic route setting

--- framework/core.go
+++ framework/core.go
@@ -3,22 +3,59 @@ package framework
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 //框架核心結構
 type Core struct{
-	router map[string]ControllerHandler
+	router map[string]map[string]ControllerHandler //二級map
 }
 
 //初始化框架核心結構
 //回傳Core引用類型
 func NewCore() *Core{
-	return &Core{router: map[string]ControllerHandler{}}
+	//定義二級map
+	getRouter := map[string]ControllerHandler{} 
+	postRouter := map[string]ControllerHandler{} 
+	putRouter := map[string]ControllerHandler{}
+	 deleteRouter := map[string]ControllerHandler{}
+
+	//將二級map寫入一級map
+	 router := map[string]map[string]ControllerHandler{} 
+	 router["GET"] = getRouter 
+	 router["POST"] = postRouter 
+	 router["PUT"] = putRouter 
+	 router["DELETE"] = deleteRouter
+	return &Core{router: router}
 }
 
+// #region 路由註冊
+
+// 對應 Method = Get
 func (c *Core) Get(url string, handler ControllerHandler) {
-	c.router[url] = handler
-}
+	upperUrl := strings.ToUpper(url)
+	c.router["GET"][upperUrl] = handler
+  }
+  
+  // 對應 Method = POST
+  func (c *Core) Post(url string, handler ControllerHandler) {
+	upperUrl := strings.ToUpper(url)
+	c.router["POST"][upperUrl] = handler
+  }
+  
+  // 對應 Method = PUT
+  func (c *Core) Put(url string, handler ControllerHandler) {
+	upperUrl := strings.ToUpper(url)
+	c.router["PUT"][upperUrl] = handler
+  }
+  
+  // 對應 Method = DELETE
+  func (c *Core) Delete(url string, handler ControllerHandler) {
+	upperUrl := strings.ToUpper(url)
+	c.router["DELETE"][upperUrl] = handler
+  }
+
+// #endregion
 
 //Core的鏈式函數宣告
 func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request){
